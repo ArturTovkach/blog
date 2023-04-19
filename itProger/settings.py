fc4c45c9ea9fb583dfeadd97fe755ec5b519ac15
Artur Tovkach <p.s.ovdok@gmail.com>
2023-04-19 12:23:16 +0200

    Modified

--- itProger/settings.py
+++ itProger/settings.py
@@ -126,6 +126,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'p.s.ovdok@gmail.com'
 EMAIL_HOST_PASSWORD = 'ixzljozdtihltpwg'
 
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
